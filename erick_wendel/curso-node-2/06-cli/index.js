fe04c4bbf7bd03f4057014d25ae73c0499f32246
Matheus Torreão <mtorreao1@gmail.com>
2021-12-19 11:55:44 -0300

    criando cli com commander

--- erick_wendel/curso-node-2/06-cli/index.js
+++ erick_wendel/curso-node-2/06-cli/index.js
@@ -1,7 +1,22 @@
+const { Command } = require('commander')
 const database = require('./database')
 
-async function main() {
-    const a = await database.obterDadosArquivo()
+const program = new Command()
 
-}
-main()
+program
+    .version('0.0.1')
+    .option('-d', 'Debug')
+
+var commandCreate = program
+    .command('create')
+    .argument('<nome>', 'Nome do herói a ser criado')
+    .argument('<poder>', 'Poder do herói a ser criado')
+    .action(async (nome, poder) => {
+        const novoHeroi = { nome, poder }
+        console.log('heroi', novoHeroi);
+
+        await database.criarHeroi(novoHeroi)
+    })
+
+
+program.parse(process.argv)
